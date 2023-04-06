b59883ce74c2a5aacd9291e0907f179a999f7355
Vladimir Surtaev <t1suvl00@students.oamk.fi>
2023-04-06 12:01:47 +0300

    Add Create Listing Activity
    It is still missing couple of screens, but at current
    condition it shouldn't block tasks with database

--- app/src/main/java/com/example/mdp_frontend/model/TopBarItem.kt
+++ app/src/main/java/com/example/mdp_frontend/model/TopBarItem.kt
@@ -1,7 +1,9 @@
 package com.example.mdp_frontend.model
 
-data class TopbarItem(
+data class TopBarItem(
     val title: String,
-    val onBackPressed: () -> Unit,
+    val drawNavUp: Boolean = false,
+    val drawClose: Boolean = false,
+    val onNavUpPressed: () -> Unit,
     val onClosePressed: () -> Unit = { /* go back to home screen */ }
 )
---
+++ app/src/main/java/com/example/mdp_frontend/model/UserModel.kt
@@ -0,0 +1,6 @@
+package com.example.mdp_frontend.model
+
+data class UserModel(
+    val name: String,
+    val imageUrl: String,
+)
