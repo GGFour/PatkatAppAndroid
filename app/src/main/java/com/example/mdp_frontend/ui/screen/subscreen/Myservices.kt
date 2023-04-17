6d8ef05bbad88425bf6c111aec2b0f3a866b9570
Mohamed Al-Hajri <c1almo00@students.oamk.fi>
2023-04-17 15:00:36 +0300

    ui for myservices and personalinfo subscreens, logic for storing user to firestore

--- app/src/main/java/com/example/mdp_frontend/ui/screen/subscreen/Myservices.kt
+++ app/src/main/java/com/example/mdp_frontend/ui/screen/subscreen/Myservices.kt
@@ -1,19 +1,99 @@
 package com.example.mdp_frontend.ui.screen.subscreen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.CheckCircle
+import androidx.compose.material.icons.filled.Schedule
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.example.mdp_frontend.model.TopBarItem
-import com.example.mdp_frontend.ui.components.TopBar
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 
 @Composable
-fun MyServices(onNavUp: () -> Unit) {
-    Column { TopBar(model = TopBarItem(
-        title = "My Services ",
-        onNavUpPressed = onNavUp
-    )
-    )
-        Text(text = "MyServices")
+fun MyServices(services: List<Service>) {
+    val colorScheme = MaterialTheme.colorScheme
+    LazyColumn {
+        items(services) { service ->
+            ServiceListItem(service = service)
+            Divider(color = colorScheme.onSurfaceVariant)
+        }
     }
+}
+
+@Composable
+fun ServiceListItem(service: Service) {
+    val colorScheme = MaterialTheme.colorScheme
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // Icon and status name
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.weight(1f)
+        ) {
+            Icon(
+                imageVector = when (service.status) {
+                    Status.Active -> Icons.Filled.CheckCircle
+                    Status.InProgress -> Icons.Filled.Schedule
+                    Status.Finished -> Icons.Filled.Cancel
+                },
+                contentDescription = null,
+                tint = when (service.status) {
+                    Status.Active -> colorScheme.primary
+                    Status.InProgress -> colorScheme.secondary
+                    Status.Finished -> colorScheme.error
+                }
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = service.status.name,
+                style = MaterialTheme.typography.bodySmall,
+                color = colorScheme.onSurfaceVariant,
+            )
+        }
+        // Name of service
+        Text(
+            text = service.name,
+            style = MaterialTheme.typography.bodyMedium,
+            color = colorScheme.onSurface,
+            modifier = Modifier.weight(1f)
+        )
+        // Price and duration of service
+        Column(horizontalAlignment = Alignment.End) {
+            Text(
+                text = "\$${service.price}",
+                style = MaterialTheme.typography.bodyMedium,
+                color = colorScheme.onSurface,
+            )
+        }
+    }
+}
+@Composable
+@Preview
+fun myServicesPreview (){
+    MyServices(services=  listOf(
+        Service(name = "House cleaning", status = Status.Active, price = 50, ),
+        Service(name = "Dog walking", status = Status.InProgress, price = 10, ),
+        Service(name = "Car wash", status = Status.Finished, price = 20,),
+        Service(name = "Lawn mowing", status = Status.Active, price = 15, ),
+        Service(name = "Grocery delivery", status = Status.InProgress, price = 25,)
+    ))
+
 }
--- app/src/main/java/com/example/mdp_frontend/ui/screen/subscreen/PersonalInfo.kt
+++ app/src/main/java/com/example/mdp_frontend/ui/screen/subscreen/PersonalInfo.kt
@@ -1,14 +1,26 @@
 package com.example.mdp_frontend.ui.screen.subscreen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ExitToApp
+import androidx.compose.material.icons.outlined.MailOutline
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberImagePainter
 import com.example.mdp_frontend.model.TopBarItem
 import com.example.mdp_frontend.ui.components.TopBar
 import com.example.mdp_frontend.viewmodel.AuthViewModel
@@ -20,36 +32,69 @@ fun PersonalInfo(viewModel: AuthViewModel = hiltViewModel(),onNavUp: () -> Unit)
         modifier = Modifier
             .fillMaxWidth()
             .wrapContentHeight()
-            .padding(16.dp)
-            .padding(top = 56.dp),
+            .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally
 
     ) {
-        Column{
-            Row {
-                Text(text ="Name" )
+        TopBar(model = TopBarItem(
+            title = "Personal Info ",
+            onNavUpPressed = onNavUp
+        )
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        // Use CircleAvatar to display the profile picture with a border and a placeholder image
+        Image(
+            painter = rememberImagePainter(viewModel?.currentUser?.photoUrl),
+            contentDescription = "Profile picture",
+            modifier = Modifier
+                .size(80.dp)
+                .clip(CircleShape)
+                .border(2.dp, MaterialTheme.colorScheme.onSurface, CircleShape)
+        )
+        // Use Spacer for vertical spacing
+        Spacer(modifier = Modifier.height(16.dp))
 
-                Text(text =viewModel?.currentUser?.displayName?:"")
-            }
+        // Use Text with a large font size and bold style to display the user name
+        Text(
+            text = viewModel?.currentUser?.displayName ?: "",
+            style = MaterialTheme.typography.headlineSmall.copy(fontWeight = FontWeight.Bold)
+        )
+        // Use Icon with a mail outline to display the user email
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.MailOutline,
+                contentDescription = "Email",
+                tint = MaterialTheme.colorScheme.primary
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(
+                text = viewModel?.currentUser?.email ?: "",
+                style = MaterialTheme.typography.bodyMedium
+            )
+        }
 
-            Row() {
-                Text(text = "Email")
-
-                Text(text =viewModel?.currentUser?.email?:"")
-            }
-            // logout button
-                Button(
-                    onClick = {
-                        viewModel?.logout()
-                        // navigate to login screen
-                        // TODO navigate to login screen or launch AuthActivity
-                    },
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp, vertical = 12.dp)
-                ) {
-                    Text(text = "Logout")
-                }
+        Spacer(modifier = Modifier.height(16.dp))
+        Divider()
+        Button(
+            onClick = {
+                viewModel?.logout()
+                // navigate to login screen
+                // TODO navigate to login screen or launch AuthActivity
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 12.dp),
+            // Use OutlinedButton for a different style
+            colors = ButtonDefaults.outlinedButtonColors()
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.ExitToApp,
+                contentDescription = "Logout"
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = "Logout")
         }
     }
 }
