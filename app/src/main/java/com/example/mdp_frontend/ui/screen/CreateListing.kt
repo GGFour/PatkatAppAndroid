c802008222d6c1a6dfa68bebd19331f21b8a91ec
Vladimir Surtaev <t1suvl00@students.oamk.fi>
2023-04-18 13:54:49 +0300

    Introduce location into listing

--- app/src/main/java/com/example/mdp_frontend/ui/screen/CreateListing.kt
+++ app/src/main/java/com/example/mdp_frontend/ui/screen/CreateListing.kt
@@ -1,10 +1,14 @@
 package com.example.mdp_frontend.ui.screen
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.net.Uri
+import android.os.Looper
+import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.StringRes
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
@@ -16,13 +20,14 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -38,6 +43,10 @@ import com.example.mdp_frontend.model.TopBarItem
 import com.example.mdp_frontend.ui.components.TopBar
 
 import com.example.mdp_frontend.viewmodel.CreateListingFormViewModel
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
 
 enum class CreateListing(@StringRes val title: Int) {
     Title(title = R.string.create_listing_title_title),
@@ -119,16 +128,25 @@ fun CreateListingScreen (
                 }
                 composable(CreateListing.Price.name) {
                     SelectPriceScreen(
-                        value= uiState.value.priceStr,
+                        value = uiState.value.priceStr,
                         onValueChanged = { viewModel.updatePrice(it) },
-                        onNextPressed = { navController.navigate(CreateListing.Review.name) },
+                        onNextPressed = { navController.navigate(CreateListing.Location.name) },
                         onCancelPressed = { cancelAndNavigateToStart(viewModel, navController) },
                         title = stringResource(id = currentScreen.title),
                     )
                 }
             //  TODO:
             //  upload listing photo
-            //  composable(CreateListing.Location.name) {}
+                composable(CreateListing.Location.name) {
+                    GetLocationScreen(
+                        latitude = uiState.value.latitude ?: 0.0,
+                        longitude = uiState.value.longitude ?: 0.0,
+                        updateLatitude = { viewModel.updateLatitude(it) },
+                        updateLongitude = { viewModel.updateLongitude(it) },
+                        onNextPressed = { navController.navigate(CreateListing.Review.name) },
+                        onCancelPressed = { cancelAndNavigateToStart(viewModel, navController) },
+                    )
+                }
                 composable(CreateListing.Review.name) {
                     ReviewListingScreen(
                         listing = uiState.value,
@@ -318,6 +336,116 @@ private fun PickCategoryScreen(
                 }
             }
 
+        }
+    }
+}
+
+@Composable
+private fun GetLocationScreen(
+    latitude: Double,
+    longitude: Double,
+    updateLatitude: (Double) -> Unit,
+    updateLongitude: (Double) -> Unit,
+    onNextPressed: () -> Unit,
+    onCancelPressed: () -> Unit,
+) {
+    val context = LocalContext.current
+    var latInput: String by remember { mutableStateOf(latitude.toString()) }
+    var lonInput: String by remember { mutableStateOf(longitude.toString()) }
+
+    // Location hell -- needs to be refactored, but not now
+    val permissions = arrayOf(
+        Manifest.permission.ACCESS_COARSE_LOCATION,
+        Manifest.permission.ACCESS_FINE_LOCATION
+    )
+    val fusedLocationClient = LocationServices.getFusedLocationProviderClient(context)
+    val locationCallback = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult) {
+            for (lo in p0.locations) {
+                // Update UI with location data
+                updateLatitude(lo.latitude)
+                latInput = lo.latitude.toString()
+                updateLongitude(lo.longitude)
+                lonInput = lo.longitude.toString()
+            }
+        }
+    }
+    val launcherMultiplePermissions = rememberLauncherForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissionsMap ->
+        val areGranted = permissionsMap.values.reduce { acc, next -> acc && next }
+        if (areGranted) {
+            Toast.makeText(context, "Permission Granted", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(context, "Permission Denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+    @SuppressLint("MissingPermission")
+    fun startLocationUpdates() {
+        locationCallback.let {
+            val locationRequest = LocationRequest.create().apply {
+                interval = 10000
+                fastestInterval = 5000
+                priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+            }
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                launcherMultiplePermissions.launch(permissions)
+                return
+            }
+            fusedLocationClient.requestLocationUpdates(
+                locationRequest,
+                it,
+                Looper.getMainLooper()
+            )
+        }
+    }
+
+    ListingScreenContainer(onNextPressed = onNextPressed, onCancelPressed = onCancelPressed) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            OutlinedTextField(
+                value = latInput,
+                onValueChange = {
+                    latInput = it.replace(',', '.')
+                    updateLatitude(latInput.toDoubleOrNull() ?: 0.0)
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                label = { Text("Latitude") }
+            )
+            Spacer(modifier = Modifier.height(15.dp))
+            OutlinedTextField(
+                value = lonInput,
+                onValueChange = {
+                    lonInput = it.replace(',', '.')
+                    updateLongitude(lonInput.toDoubleOrNull() ?: 0.0)
+                },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                label = { Text("Longitude") }
+            )
+            Button(onClick = {
+                if (permissions.all {
+                        ContextCompat.checkSelfPermission(
+                            context,
+                            it
+                        ) == PackageManager.PERMISSION_GRANTED
+                    }) {
+                    // Get the location
+                    startLocationUpdates()
+                } else {
+                    launcherMultiplePermissions.launch(permissions)
+                }
+            }) {
+                Text(text = "Get current location")
+            }
         }
     }
 }
